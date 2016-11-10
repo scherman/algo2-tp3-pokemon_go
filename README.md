811125de0426eb543408bb1a4073069a612eeda2
Jonathan Scherman <jonischerman@gmail.com>
2016-11-10 20:35:48 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+Implementación segun lo diseñado [Acá](https://www.overleaf.com/6510328bqsjcx#/22000340/): 
+
---
+++ TiposJuego.h
@@ -0,0 +1,27 @@
+#ifndef TIPOSJUEGO_H_
+#define TIPOSJUEGO_H_
+
+#include "aed2/TiposBasicos.h"
+
+typedef unsigned int Jugador;
+typedef std::string Pokemon;
+using namespace aed2;
+ 
+struct Coordenada
+{
+	Nat latitud;
+	Nat longitud;
+	Coordenada(Nat latitud, Nat longitud) : latitud(latitud), longitud(longitud)
+	{
+	}
+};
+
+bool operator == (const Coordenada& c1, const Coordenada& c2){
+ return c1.latitud == c2.latitud and c1.longitud == c2.longitud;
+}
+
+bool operator != (const Coordenada& c1, const Coordenada& c2){
+ return not (c1 == c2);
+}
+
+#endif
